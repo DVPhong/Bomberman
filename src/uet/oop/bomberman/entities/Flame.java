333b8338bb358ad90004a150db8f6b4c62e33f9d
DVPhong <21020375@vnu.edu.vn>
2022-10-30 21:14:41 +0700

    Fix Flame

--- src/uet/oop/bomberman/entities/Flame.java
+++ src/uet/oop/bomberman/entities/Flame.java
@@ -1,7 +1,6 @@
 package uet.oop.bomberman.entities;
 
 import javafx.scene.image.Image;
-import uet.oop.bomberman.BombermanGame;
 import uet.oop.bomberman.graphics.Map;
 import uet.oop.bomberman.graphics.Sprite;
 
@@ -10,7 +9,7 @@ public class Flame extends Entity{
     public Flame(int xUnit, int yUnit, Image img) {
         super(xUnit, yUnit, img);
         this.x = xUnit * Sprite.SCALED_SIZE;
-        if (yUnit >= 3 && yUnit <= Map.c) this.y = yUnit * Sprite.SCALED_SIZE;
+        if (yUnit >= 3 && yUnit <= Map.r) this.y = yUnit * Sprite.SCALED_SIZE;
         else if (yUnit < 3) this.y = 3 * Sprite.SCALED_SIZE;
         else this.y = (Map.r) * Sprite.SCALED_SIZE;
         this.img = img;
